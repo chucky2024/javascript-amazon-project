66854b1e9709c51ddd505cf3badbce60bcf96722
Chukwuebuka <cokeke678@gmail.com>
2025-08-14 11:45:10 +0100

    Made the "cart quantity" interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,7 +77,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
          quantity: 1 
       });
       }
+
+      let cartQuantity = 0;
+      cart.forEach((item) => {
+            cartQuantity += item.quantity;
+      })
       
-      console.log(cart);
+      document.querySelector('.js-cart-quantity').innerHTML = `${cartQuantity}`;
    });
 });
